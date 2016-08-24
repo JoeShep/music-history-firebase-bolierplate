cc5e61d8655923bd62bf6987f6d1898b37ab5ed6
Joe Shepherd <joeshepmedia@gmail.com>
2016-08-23 20:09:04 -0500

    Remove SDK refs

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let $ = require('jquery'),
 function loadSongsToDOM() {
   console.log("Need to load some songs, Buddy");
 }
-loadSongsToDOM();
+loadSongsToDOM(); //<--Move to auth section after adding login btn
 
 // Send newSong data to db then reload DOM with updated song data
 $(document).on("click", ".save_new_btn", function() {
@@ -32,40 +32,13 @@ $(document).on("click", ".delete-btn", function () {
 });
 
 
-// *******************************************************
-// Using the Firebase SDK and its auto update.
-// Calling "getSongs" now sets up a listener when loading the DOM.
-// Pass it a callback that handles the incoming data and it will re-call that
-// function when anything changes in our data
-
-// Use before adding Oauth, then move to Oauth callback
-// db.getSongs(templates.makeSongList);
-
-// No need to reload the DOM in the ".then"
-// $(document).on("click", ".save_new_btn", function() {
-
-// });
-
-// // Load and populate form for editing a song
-// $(document).on("click", ".edit-btn", function () {
-
-// });
-
-// //Save edited song to FB then reload DOM with updated song data
-// $(document).on("click", ".save_edit_btn", function() {
-
-// });
-
-// // Remove song then reload the DOM w/out new song
-// $(document).on("click", ".delete-btn", function () {
-
-// });
-
+//***************************************************************
 // User login section. Should ideally be in its own module
 // $("#auth-btn").click(function() {
 //   console.log("clicked auth");
 
 // });
+//****************************************************************
 
 // Helper functions for forms stuff. Nothing related to Firebase
 // Build a song obj from form data.
