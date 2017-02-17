686c557378cbbcad83e08e4d31382799c11351f1
Brenda Long <brenda@studio6thave.com>
2017-02-17 08:20:52 -0600

    remove values

--- javascripts/fb-getter-example.js
+++ javascripts/fb-getter-example.js
@@ -3,9 +3,9 @@
 
 function getKey() {
   return {
-    apiKey: "AIzaSyCf7vkHSHECTJhUDHvIaBZvCufZsqJsm9Q",
-    authDomain: "musichistory-d16.firebaseapp.com",
-    databaseURL: "https://musichistory-d16.firebaseio.com",
+    apiKey: "reallyLongValue",
+    authDomain: "something.firebaseapp.com",
+    databaseURL: "https://something.firebaseio.com"
   };
 }
 //something is your fb address
