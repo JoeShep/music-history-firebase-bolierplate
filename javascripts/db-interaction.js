13a86974b2db5e97893782055aa08a4582969f83
Brenda Long <brenda@studio6thave.com>
2018-03-01 05:32:51 -0600

    changes related file names

--- javascripts/db-interaction.js
+++ javascripts/db-interaction.js
@@ -11,29 +11,22 @@ let $ = require('jquery'),
 //check on crossDomain: true
 
 function getSongs(user) {
-	return new Promise((resolve, reject) => {
-		console.log("url", firebase.getFBsettings().databaseURL);
-		$.ajax({
-			url: `${firebase.getFBsettings().databaseURL}/songs.json?orderBy="uid"&equalTo="${user}"`
-
-			// url: `https://musichistory-d16.firebaseio.com/songs.json?orderBy="uid"&equalTo="${user}"`
-		}).done((songData) => {
-			console.log("songData in promise", songData);
-			resolve(songData);
-		}).fail((error) => {
-			reject(error);
-		});
-	});
+   console.log("url", firebase.getFBsettings().databaseURL);
+	return $.ajax({
+		url: `${firebase.getFBsettings().databaseURL}/songs.json?orderBy="uid"&equalTo="${user}"`
+		// url: `https://musichistory-d16.firebaseio.com/songs.json?orderBy="uid"&equalTo="${user}"`
+	}).done((songData) => {
+		console.log("songData in promise", songData);
+		return songData;
+   });
 }
 
 //version 1 - without user id /////////////////////////////////
 // function getSongs() {
-// 	return new Promise((resolve, reject) => {
-// 		$.ajax({
-// 			url: `${firebase.getFBsettings().databaseURL}/songs.json`
-// 		}).done((songData) => {
-// 			resolve(songData);
-// 		});
+// 	return $.ajax({
+// 		url: `${firebase.getFBsettings().databaseURL}/songs.json`
+// 	}).done((songData) => {
+// 		return songData;
 // 	});
 // }
 
@@ -41,55 +34,47 @@ function getSongs(user) {
 
 function addSong(songFormObj) {
 	console.log("addSong", songFormObj);
-	return new Promise((resolve, reject) => {
-		$.ajax({
-			url: `${firebase.getFBsettings().databaseURL}/songs.json`,
-			type: 'POST',
-			data: JSON.stringify(songFormObj),
-			dataType: 'json'
-		}).done((songID) => {
-			resolve(songID);
-		});
-	});
+	return $.ajax({
+      url: `${firebase.getFBsettings().databaseURL}/songs.json`,
+      type: 'POST',
+      data: JSON.stringify(songFormObj),
+      dataType: 'json'
+   }).done((songID) => {
+      return songID;
+   });
 }
 // POST - Submits data to be processed to a specified resource.
 
 
 function deleteSong(songId) {
-	return new Promise((resolve, reject) => {
-		$.ajax({
-			url: `${firebase.getFBsettings().databaseURL}/songs/${songId}.json`,
-			method: "DELETE"
-		}).done(() => {
-			resolve();
-		});
+	$.ajax({
+      url: `${firebase.getFBsettings().databaseURL}/songs/${songId}.json`,
+      method: "DELETE"
+	}).done((data) => {
+		return data;
 	});
 }
 
 function getSong(songId) {
-	return new Promise((resolve, reject) => {
-		$.ajax({
-			url: `${firebase.getFBsettings().databaseURL}/songs/${songId}.json`
-		}).done((songData) => {
-			resolve(songData);
-		}).fail((error) => {
-			reject(error);
-		});
-	});
+   return $.ajax({
+      url: `${firebase.getFBsettings().databaseURL}/songs/${songId}.json`
+   }).done((songData) => {
+      return songData;
+   }).fail((error) => {
+      return error;
+   });
 }
 
 // GET - Requests/read data from a specified resource
 // PUT - Update data to a specified resource.
 function editSong(songFormObj, songId) {
-	return new Promise((resolve, reject) => {
-		$.ajax({
-			url: `${firebase.getFBsettings().databaseURL}/songs/${songId}.json`,
-			type: 'PUT',
-			data: JSON.stringify(songFormObj)
-		}).done((data) => {
-			resolve(data);
-		});
-	});
+   return $.ajax({
+      url: `${firebase.getFBsettings().databaseURL}/songs/${songId}.json`,
+      type: 'PUT',
+      data: JSON.stringify(songFormObj)
+   }).done((data) => {
+      return data;
+   });
 }
 
 module.exports = {
